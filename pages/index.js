7e816502a9e4c42ac37e0949faf59b73dd54c255
Giulian <giulian.franco@gmail.com>
2023-05-02 11:21:54 -0300

    Beleza

--- pages/index.js
+++ pages/index.js
@@ -26,29 +26,24 @@ export default function Home({ pokemons }) {
   return (
     <>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-        <h1>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script> 
+          <div class="container">
+          <h1>
           Poke<span>Next</span>
         </h1>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Id-Pokedex</th>
-              <th scope="col">Nome</th>
-            </tr>
-          </thead>
-          <tbody>
           {pokemons.map((pokemon) => (
-              <tr><Link href={`/pokemon/${pokemon.id}`} >
-                <th scope="row"><img src={pokemon.image} width='30' height='30'></img></th>
-                <td><p>#{pokemon.id}</p></td>
-                <td>{pokemon.name}</td>
+              <Link href={`/pokemon/${pokemon.id}`} >
+                <div class="row card">
+                <div class='col card-body justify-content-center'>
+                <img src={pokemon.image} width='100' height='100'></img>
+                <h1>#{pokemon.id}: {pokemon.name}</h1>
+                </div>
+                </div>
               </Link>
-              </tr>
+              
           ))}
-          </tbody>
-        </table>
+        </div>
+        
       
     </>
   );
--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -46,35 +46,84 @@ export default function PokemonPage({ pokemon }) {
     <>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-      <div>
-        <img src={image} width='200' height='200'></img>
-        <h1>
-          {pokemon.id}
-          {pokemon.name}
-        </h1>
 
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-4 card">
+            <div class="card-body">
+              <h1> {pokemon.name} </h1>
+            </div>
+          </div>
+          <div class="col-1 card">
+            <div class="card-body">
+              <h1>  {pokemon.id}</h1>
+            </div>
+          </div>
+          <div class="col card">
+            <div class="card-body">
+              <h1> <strong>Habilidades</strong></h1>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-5 card">
+            <div class="card-body">
+              <img src={image} width='400' height='400'></img>
+            </div>
+          </div>
+          <div class="col-7 card">
+            <div class="card-body">
+              {
+                pokemon.abilities.map((item) => (
+                  <h1>
+                    {item.ability.name}
+                  </h1>
+                ))
+              }
+            </div>
+          </div>
+          <div class="col card">
+            <div class="card-body">
+              <strong>Status</strong>
+              {
+                pokemon.stats.map((item) => (
+                  <h1>
+                    {item.stat.name}: {item.base_stat}
+                  </h1>
+                ))
+              }
+            </div>
+          </div>
+          <div class="col card">
+            <div class="card-body">
+              <strong>Tipo</strong>
+
+
+              {
+                pokemon.types.map((item) => (
+                  <h1>
+                    {item.type.name}
+                  </h1>
+                ))
+              }
+            </div>
+          </div>
+        </div>
       </div>
-      {pokemon.types.map((item) => (
-        <h1>
-          {item.type.name}
-        </h1>
-      ))}
-
-      {pokemon.abilities.map((item) => (
-        <h1>
-          {item.ability.name}
-        </h1>
-      ))}
-
-      {pokemon.stats.map((item) => (
-        <h1>
-          {item.stat.name}: {item.base_stat}
-        </h1>
-      ))}
-      <h1>
-      <Link href={`/pokemon/${pokemon.id-1}`} ><button>voltar</button> </Link>
-      <Link href={`/pokemon/${pokemon.id+1}`} ><button>prox</button></Link>
-      </h1>
+      <div class='container'>
+        <div class=" row">
+          <div class="col-5">
+            <a href={`/pokemon/${pokemon.id - 1}`}><button type='button' class='btn btn-primary'>Anterior</button></a>
+          </div>
+          <div class='col-6'>
+          <a href={`../`}><button type='button' class='btn btn-primary'>Inicio</button></a>
+          </div>
+          <div class=" col-1 justify-content-end">
+            <a href={`/pokemon/${pokemon.id + 1}`}><button type='button' class='btn btn-primary'>Proximo</button></a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
